cc61b76ea689efcd9afc14f03f773f946482e34e
Vasily Ivanov <vasily.ivanov.jukebox@gmail.com>
2016-02-17 14:02:13 +0300

    Test app for HandleView has been added

---
+++ Common/HandleViewTest/HandleTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HandleTest
+//
+//  Created by Vasily Ivanov on 17.02.16.
+//  Copyright © 2016 Vasily Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
